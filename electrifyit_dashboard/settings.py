787ee8f6890bff3897b5eaef394cb8e0919fbebd
Jency Kachhadiya <jencykachhadiya51@gmail.com>
2024-04-10 12:00:45 +0530

    Changes on settings.py

--- electrifyit_dashboard/settings.py
+++ electrifyit_dashboard/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-614os%n-i^rp(3ic*y&()kbtv5djwvxii05ahz5!*qr@8l!w*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
