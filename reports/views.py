893355114d6fac5102a482dedfec920d39cfe59a
Jency Kachhadiya <jencykachhadiya51@gmail.com>
2024-04-10 14:45:06 +0530

    Changes on settings.py

--- reports/views.py
+++ reports/views.py
@@ -38,32 +38,34 @@ def detailed_report(request):
         end_date_str = request.POST.get('endDate')
         group_by = request.POST.get('groupBy')
 
-        try:
-            start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
-            end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+        # Check if start_date_str and end_date_str are not None
+        if start_date_str and end_date_str:
+            try:
+                start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+                end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
 
-            if group_by == 'daily':
-                report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
-                    .values('date') \
-                    .annotate(total_miles=Sum('miles_driven'))
-            elif group_by == 'weekly':
-                report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
-                    .annotate(week=ExtractWeek('date')) \
-                    .values('week') \
-                    .annotate(total_miles=Sum('miles_driven'))
-            elif group_by == 'monthly':
-                report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
-                    .annotate(month=ExtractMonth('date')) \
-                    .values('month') \
-                    .annotate(total_miles=Sum('miles_driven'))
-            elif group_by == 'yearly':
-                report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
-                    .annotate(year=ExtractYear('date')) \
-                    .values('year') \
-                    .annotate(total_miles=Sum('miles_driven'))
+                if group_by == 'daily':
+                    report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
+                        .values('date') \
+                        .annotate(total_miles=Sum('miles_driven'))
+                elif group_by == 'weekly':
+                    report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
+                        .annotate(week=ExtractWeek('date')) \
+                        .values('week') \
+                        .annotate(total_miles=Sum('miles_driven'))
+                elif group_by == 'monthly':
+                    report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
+                        .annotate(month=ExtractMonth('date')) \
+                        .values('month') \
+                        .annotate(total_miles=Sum('miles_driven'))
+                elif group_by == 'yearly':
+                    report_data = Vehicle.objects.filter(date__range=[start_date, end_date]) \
+                        .annotate(year=ExtractYear('date')) \
+                        .values('year') \
+                        .annotate(total_miles=Sum('miles_driven'))
 
-        except (ValueError, Vehicle.DoesNotExist):
-            report_data = None  
+            except (ValueError, Vehicle.DoesNotExist):
+                report_data = None
 
     return render(request, 'detailed_report.html', {'report_data': report_data})
 
@@ -72,8 +74,8 @@ def date_range_report(request):
     data = None
     
     if request.method == 'POST':
-        start_date_str = request.POST.get('start_date')
-        end_date_str = request.POST.get('end_date')
+        start_date_str = request.POST.get('startDate')
+        end_date_str = request.POST.get('endDate')
         
         try:
             start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
